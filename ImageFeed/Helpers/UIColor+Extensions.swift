1d144dc535686dad6f9f35ab6008e1313ac5b913
FierySasquatch168 <9120603@gmail.com>
2022-12-17 19:55:44 +0200

    no message

---
+++ ImageFeed/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extensions.swift
+//  ImageFeed
+//
+//  Created by Aleksandr Eliseev on 17.12.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlack: UIColor? { UIColor(named: "YPBlack")}
+}
